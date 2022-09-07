8a4a77e306579956b8069f085dd00a12d29bd376
Raul Rothschild <raultonello18@gmail.com>
2022-09-07 15:37:21 -0300

    refactor: patientForm refactor

--- src/pages/patients/patientCreate.vue
+++ src/pages/patients/patientCreate.vue
@@ -8,165 +8,77 @@
         </div>
       </template>
     </f7-navbar>
-    <f7-block class="mt-3">
+    <f7-block class="mt-3 mb-2">
       <section class="form">
         <h5 class="fw-bold">Passos:</h5>
 
         <Stepper v-bind="stepperConfig" />
 
         <f7-tabs animated>
-          <f7-tab id="tab-1" tab-active>
+          <f7-tab id="tab-1" tab-active v-if="stepperConfig.currentStep === 0">
             <!-- STEP 1 -->
             <f7-row>
-              <f7-col width="100" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input
-                    label="Nome"
-                    floating-label
-                    outline
-                    type="text"
-                    v-model:value="values.name"
-                    clear-button
+              <h5>Dados basicos</h5>
+              <form>
+                <f7-row v-if="hasFields">
+                  <f7-col
+                    :width="patientFirstStepColumns[fieldName]"
+                    class="mb-3"
+                    v-for="(fieldName, key) of patientFirstStepFields"
+                    :key="key"
                   >
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="100" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input
-                    label="Tipo ocupação"
-                    floating-label
-                    type="select"
-                    v-model:value="values.occupation"
-                    outline
-                  >
-                    <option value="-">-</option>
-                    <option value="Aposentado">Aposentado</option>
-                    <option value="Detento">Detento</option>
-                    <option value="Desempragado">Desempragado</option>
-                    <option value="Profissional">Profissional</option>
-                    <option value="Profissional_de_Saúde">Profissional de Saúde</option>
-                    <option value="Profissional_Sistema_Penitenciário">Profissional Sistema Penitenciário</option>
-                    <option value="Outra">Outra</option>
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="50" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input label="Faixa Etaria" floating-label type="select" v-model:value="values.age" outline>
-                    <option value="-">-</option>
-                    <option value="0">Menor de 1 ano</option>
-                    <option value="01_04">1 a 4</option>
-                    <option value="05_09">5 a 9</option>
-                    <option value="10_14">10 a 14</option>
-                    <option value="15_19">15 a 19</option>
-                    <option value="20_29">20 a 29</option>
-                    <option value="30_39">30 a 39</option>
-                    <option value="40_49">40 a 49</option>
-                    <option value="50_59">50 a 59</option>
-                    <option value="60_69">60 a 69</option>
-                    <option value="70_79">70 a 79</option>
-                    <option value="-1">Maior de 80 anos</option>
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="50" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input label="Raça/Cor" floating-label type="select" v-model:value="values.race" outline>
-                    <option value="-">-</option>
-                    <option value="Amarelo">Amarelo</option>
-                    <option value="Branco">Branco</option>
-                    <option value="Indigena">Indigena</option>
-                    <option value="Pardo">Pardo</option>
-                    <option value="Preto">Preto</option>
-                    <option value="Ignorado">Ignorado</option>
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="50" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input label="Sexo" floating-label type="select" v-model:value="values.gender" outline>
-                    <option value="-">-</option>
-                    <option value="M">Masculino</option>
-                    <option value="F">Feminino</option>
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="50" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input
-                    label="Escolaridade"
-                    floating-label
-                    type="select"
-                    v-model:value="values.schooling"
-                    outline
-                  >
-                    <option value="-">-</option>
-                    <option value="1_3">De 1 a 3 anos</option>
-                    <option value="4_7">De 4 a 7 anos</option>
-                    <option value="8_11">De 8 a 11 anos</option>
-                    <option value="12_14">De 12 a 14 anos</option>
-                    <option value="15">15 ou mais anos</option>
-                    <option value="Ignorado">Ignorado</option>
-                    <option value="Nenhuma">Nenhuma</option>
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="100" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input
-                    label="Código do tratamento anterior"
-                    floating-label
-                    type="select"
-                    v-model:value="values.lastTreatmentCode"
-                    outline
-                  >
-                    <option value="new">Novo. Não tratou</option>
-                    <option value="return">Sim, alta cura. Reicidiva</option>
-                    <option value="returnAbandon">Retorno abandono</option>
-                    <option value="retreatmentAbandon">Sim, alta abandono.</option>
-                    <option value="unknow">Não sabe</option>
-                    <option value="death">Obito</option>
-                    <option value="noData">Sem informação</option>
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="100" class="mb-3">
-                <f7-list no-hairlines class="m-0">
-                  <f7-list-input
-                    label="Situação atual"
-                    floating-label
-                    type="select"
-                    v-model:value="values.currentSituation"
-                    outline
-                  >
-                    <option value="abandon">Abandono</option>
-                    <option value="primaryAbandon">Abandono Primário</option>
-                    <option value="healed">Cura</option>
-                    <option value="inOutpatientTreatment">Em tratamento ambulatorial</option>
-                    <option value="hospitalized">Em Tratamento Internado</option>
-                    <option value="deathOrResistence">Falência/Resistência</option>
-                    <option value="absentee">Faltoso</option>
-                    <option value="mudDiag">Mud Diag</option>
-                    <option value="mudDiag2">Mud Esquema Intoler/Toxicidade</option>
-                    <option value="nonTBDeath">Óbito NTB</option>
-                    <option value="TBDeath">Óbito TB</option>
-                  </f7-list-input>
-                </f7-list>
-              </f7-col>
-              <f7-col width="100" class="mb-3">
-                <f7-button fill class="Button w-100 mb-2" tab-link="#tab-2" @click="nextStep()">
-                  <span class="text-capitalize">Prosseguir</span>
-                </f7-button>
-                <pre>{{ values }}</pre>
-              </f7-col>
+                    <f7-list no-hairlines class="m-0" v-if="handleInputCheckType(fieldName)">
+                      <f7-list-input
+                        :name="fields[fieldName].name"
+                        :label="fields[fieldName].label"
+                        :type="fields[fieldName].type"
+                        :placeholder="fields[fieldName].label"
+                        v-model:value="values[fields[fieldName].name]"
+                        floating-label
+                        outline
+                      >
+                        <option
+                          v-for="(option, optionKey) of fields[fieldName].options"
+                          :key="optionKey"
+                          :value="option.value"
+                        >
+                          {{ option.label }}
+                        </option>
+                      </f7-list-input>
+                    </f7-list>
+
+                    <f7-list no-hairlines class="m-0" v-if="isRadio(fieldName)">
+                      <small class="w-100 fw-bold d-block">{{ fields[fieldName].label }}</small>
+
+                      <div
+                        v-for="(radio, radioKey) of fields[fieldName].options"
+                        :key="radioKey"
+                        class="d-flex align-items-center mb-3"
+                      >
+                        <label :for="fields[fieldName].name" class="ms-2 me-1">{{ radio.label }}</label>
+                        <f7-radio
+                          :name="fields[fieldName].name"
+                          :value="radio.value"
+                          :checked="true"
+                          @change="handleRadioChange($event, fieldName)"
+                        >
+                        </f7-radio>
+                      </div>
+                    </f7-list>
+                  </f7-col>
+                  <f7-col width="100">
+                    <f7-button fill class="Button w-100" tab-link="#tab-2" @click="nextStep()">
+                      <span class="text-capitalize">Prosseguir</span>
+                    </f7-button>
+                  </f7-col>
+                </f7-row>
+              </form>
             </f7-row>
           </f7-tab>
 
-          <f7-tab id="tab-2">
-            <!-- STEP 2 -->
+          <f7-tab id="tab-2" v-if="stepperConfig.currentStep === 1">
             <f7-row>
-              <h2>Dados médicos</h2>
+              <h5>Dados médicos</h5>
 
               <form>
                 <f7-row v-if="hasFields">
@@ -206,14 +118,14 @@
                 <f7-button outline class="Button w-100" tab-link="#tab-1" @click="backStep()">
                   <span class="text-capitalize">Voltar</span>
                 </f7-button>
-                <pre>{{ values }}</pre>
               </f7-col>
             </f7-row>
           </f7-tab>
 
-          <f7-tab id="tab-3">
+          <f7-tab id="tab-3" v-if="stepperConfig.currentStep === 2">
             <f7-row>
-              <h2>Dados sobre a TB e comorbidades</h2>
+              <h5>Dados sobre a TB e comorbidades</h5>
+
               <form>
                 <f7-row v-if="hasFields">
                   <f7-col
@@ -242,15 +154,19 @@
                       </f7-list-input>
                     </f7-list>
 
-                    <f7-list v-if="fields[fieldName].type === 'radio'">
-                      <small>{{ fields[fieldName].label }}</small>
+                    <f7-list no-hairlines class="m-0" v-if="isRadio(fieldName)">
+                      <small class="w-100 fw-bold d-block">{{ fields[fieldName].label }}</small>
 
-                      <div v-for="(radio, radioKey) of fields[fieldName].options" :key="radioKey">
-                        <small>{{ radio.label }}</small>
+                      <div
+                        v-for="(radio, radioKey) of fields[fieldName].options"
+                        :key="radioKey"
+                        class="d-flex align-items-center mb-3"
+                      >
+                        <label :for="fields[fieldName].name" class="ms-2 me-1">{{ radio.label }}</label>
                         <f7-radio
                           :name="fields[fieldName].name"
                           :value="radio.value"
-                          checked
+                          :checked="true"
                           @change="handleRadioChange($event, fieldName)"
                         >
                         </f7-radio>
@@ -258,17 +174,16 @@
                     </f7-list>
                   </f7-col>
                 </f7-row>
-              </form>
 
-              <f7-col width="100">
-                <f7-button fill class="Button w-100" @click="createPatient()">
-                  <span class="text-capitalize">Finalizar</span>
-                </f7-button>
-                <f7-button outline class="Button w-100" tab-link="#tab-2" @click="backStep()">
-                  <span class="text-capitalize">Voltar</span>
-                </f7-button>
-                <pre>{{ values }}</pre>
-              </f7-col>
+                <f7-col width="100">
+                  <f7-button fill class="Button w-100" @click="createPatient()">
+                    <span class="text-capitalize">Finalizar</span>
+                  </f7-button>
+                  <f7-button outline class="Button w-100" tab-link="#tab-2" @click="backStep()">
+                    <span class="text-capitalize">Voltar</span>
+                  </f7-button>
+                </f7-col>
+              </form>
             </f7-row>
           </f7-tab>
         </f7-tabs>
@@ -366,13 +281,13 @@ export default {
       return [
         "name",
         "occupation",
+        "lastTreatmentCode",
         "age",
         "race",
         "gender",
         "schooling",
-        "currentSituation",
-        "lastTreatmentCode",
         "lastTreatment",
+        "currentSituation",
         "totComunic",
         "totComunicEx",
         "totComunicAd",
@@ -380,7 +295,20 @@ export default {
     },
 
     patientFirstStepColumns() {
-      return {};
+      return {
+        name: 100,
+        occupation: 100,
+        age: 50,
+        race: 50,
+        gender: 50,
+        schooling: 50,
+        currentSituation: 50,
+        lastTreatmentCode: 100,
+        lastTreatment: 50,
+        totComunic: 100,
+        totComunicEx: 100,
+        totComunicAd: 100,
+      };
     },
 
     patientSecondStepFields() {
@@ -421,21 +349,21 @@ export default {
 
     patientThirdStepFields() {
       return [
-        "hiv",
-        "diabetes",
-        "alcoolismo",
-        "mental",
-        "drogadicao",
-        "tabagismo",
-        "aids",
         "sensibility",
         "treatment",
         "initialExposure",
-        "exposureChange",
-        "actualExposure",
-        "actualExposureReason",
         "resistencia",
         "histo",
+        "actualExposure",
+        "actualExposureReason",
+        "exposureChange",
+        "hiv",
+        "diabetes",
+        "alcoolismo",
+        "drogadicao",
+        "tabagismo",
+        "aids",
+        "mental",
       ];
     },
 
@@ -516,7 +444,15 @@ export default {
     },
 
     handleInputCheckType(fieldName) {
-      return this.fields[fieldName].type === "input" || this.fields[fieldName].type === "select";
+      return (
+        this.fields[fieldName].type === "input" ||
+        this.fields[fieldName].type === "select" ||
+        this.fields[fieldName].type === "text"
+      );
+    },
+
+    isRadio(fieldName) {
+      return this.fields[fieldName].type === "radio";
     },
   },
 };
