0b8585b08e29a3bce959a1adefab9f6f4a934921
Raul Rothschild <raultonello18@gmail.com>
2022-09-14 21:05:37 -0300

    hotfix: https SSL

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@ const MODE = import.meta.env.MODE;
 const BASE_URL = import.meta.env.BASE_URL;
 
 const api = axios.create({
-  baseURL: MODE === "development" ? "http://localhost:3000/" : "http://34.95.223.152:3000/",
+  baseURL: MODE === "development" ? "http://localhost:3000/" : "https://34.95.223.152:3000/",
 });
 
 export default api;
