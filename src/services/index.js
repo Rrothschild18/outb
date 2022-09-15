d79b463e9402a802e473ead7538d943da6f39b00
Raul Rothschild <raultonello18@gmail.com>
2022-09-15 00:20:06 -0300

    dev api link

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@ const MODE = import.meta.env.MODE;
 const BASE_URL = import.meta.env.BASE_URL;
 
 const api = axios.create({
-  baseURL: MODE === "development" ? "http://34.66.72.119:3000/" : "http://34.95.223.152:3000/",
+  baseURL: MODE === "development" ? "http://34.66.72.119:3000/" : "https://outb-jsonserver.vercel.app/",
 });
 
 export default api;
